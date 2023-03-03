96de9bfe7570823a215c4a503f62a7deb9b96f10
oussamabalbal <o.balbal@esi-sba.dz>
2023-03-03 12:15:53 +0000

    Add additional pots

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,10 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+# Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
